77f1c187b1390873d247c68d58ba705313f6682f
buzhi <18325876085@163.com>
2025-08-17 20:05:19 +0800

    只提交week01作业

---
+++ main.py
@@ -0,0 +1,12 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+from workWeek1.workweek1 import week01_func
+
+
+if __name__ == '__main__':
+    week01_func()
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
